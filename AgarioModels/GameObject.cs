a9338047d873960adbba271dd5b8a5aa9a9af86e
25hlarsen4 <u1341754@umail.utah.edu>
2023-04-05 14:33:31 -0600

    Add project files.

---
+++ AgarioModels/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgarioModels
+{
+    internal class GameObject
+    {
+        int ID;
+
+        int ARGBcolor;
+
+        float mass;
+
+        // determine by considering the mass to be the area/vol
+        float radius;
+
+        Vector2 position = new Vector2(100, 100);
+
+        public float xPos
+        {
+            get { return position.X; }
+        }
+
+        public float yPos
+        {
+            get { return position.Y; }
+        }
+    }
+}
---
+++ AgarioModels/World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgarioModels
+{
+    /// <summary>
+    /// This class represents the "state" of the simulation
+    /// </summary>
+    internal class World
+    {
+        // also store a logger
+
+        readonly int width = 5000;
+        readonly int height = 5000;
+
+        HashSet<Player> players;
+        HashSet<Food> food;
+    }
+}
