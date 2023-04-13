3afee45bb3317b59c4f198c92cd84172f11ad463
25hlarsen4 <u1341754@umail.utah.edu>
2023-04-12 18:00:45 -0600

    Documented the MainPage class.

--- ClientGUI/MainPage.xaml.cs
+++ ClientGUI/MainPage.xaml.cs
@@ -7,14 +7,48 @@ using System.Timers;
 
 namespace ClientGUI
 {
+    /// <summary>
+    /// Authors:     Hannah Larsen & Todd Oldham
+    /// Date:        05-April-2023
+    /// Course:      CS3500, University of Utah, School of Computing
+    /// Copyright:   CS3500, Hannah Larsen, and Todd Oldham - This work may not be copied for use in academic coursework.
+    /// 
+    /// We, Hannah Larsen and Todd Oldham, certify that we wrote this code from scratch and did not copy it in part or whole 
+    /// from another source.
+    /// All references used in the completion of the assignment are cited in our README file.
+    /// 
+    /// File Contents:
+    /// This class provides the backing code for the agario GUI xaml code. It allows for
+    /// a client to connect to the game, play it, and either play again or quit upon their death.
+    /// </summary>
     public partial class MainPage : ContentPage
     {
+        /// <summary>
+        /// The Drawable object that allows for the game world to be continuously redrawn.
+        /// This object stores a Client object, which keeps track of "this player" playing the game
+        /// and stores a World object, which keeps track of the state of the game world.
+        /// </summary>
         Drawable drawable;
+
+        /// <summary>
+        /// The Networking object that allows this player to connect to the server and play the game.
+        /// </summary>
         Networking channel;
 
+        /// <summary>
+        /// Stores the current mouse position.
+        /// </summary>
         Point mousePosition;
+
+        /// <summary>
+        /// A stopwatch used to track the amount of time a player has stayed alive to include
+        /// in their final stats upon their death.
+        /// </summary>
         Stopwatch stopWatch;
 
+        /// <summary>
+        /// This starts the application.
+        /// </summary>
         public MainPage()
         {
             InitializeComponent();
@@ -22,6 +56,10 @@ namespace ClientGUI
             InitializationLogic();
         }
 
+        /// <summary>
+        /// This method initializes the game by initializing the Drawable, which initializes 
+        /// the Client, which initializes the World.
+        /// </summary>
         private void InitializationLogic()
         {
             drawable = new Drawable();
@@ -33,10 +71,19 @@ namespace ClientGUI
             stopWatch = new Stopwatch();
         }
 
+        /// <summary>
+        /// This method ensures that when a client enters their name, the game screen is made
+        /// visible and an attempt to connect to the server is made.
+        /// </summary>
+        /// <param name="sender"> the sender </param>
+        /// <param name="e">the event args </param>
         private async void OnNameEntryCompleted(object sender, EventArgs e)
         {
             WelcomeScreen.IsVisible = false;
             GameScreen.IsVisible = true;
+
+            // focus the entry that keeps track of keyboard input to handle when a player
+            // wants to split
             SpaceBarEntry.Focus();
 
             try
@@ -55,32 +102,42 @@ namespace ClientGUI
             }
         }
 
-        private void OnServerEntryCompleted(object sender, EventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// This method continuously updates the global mousePosition variable so we know at all 
+        /// times where to send move requests to.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PointerChanged(object sender, PointerEventArgs e)
         {
             Point windowPosition = (Point)e.GetPosition(null);
             mousePosition = windowPosition;
         }
 
+        /// <summary>
+        /// This method converts screen coordinates to world coordinates, since the mousePosition 
+        /// is in screen coordinates and must be converted to world coordinates before every request 
+        /// to the server to move toward that point.
+        /// </summary>
+        /// <param name="screen_x">  the screen x coordinate </param>
+        /// <param name="screen_y"> the screen y coordinate </param>
+        /// <param name="world_x"> the converted world x coordinate </param>
+        /// <param name="world_y"> the converted world y coordinate </param>
         private void ConvertFromScreenToWorld(in float screen_x, in float screen_y,
                                             out int world_x, out int world_y)
         {
             int leftBoundary = (int)(drawable.client.thisPlayer.X - (drawable.client.currViewPortalWidth / 2));
-
-            int rightBoundary = (int)(drawable.client.thisPlayer.X + (drawable.client.currViewPortalWidth / 2));
-
-            int bottomBoundary = (int)(drawable.client.thisPlayer.Y + (drawable.client.currViewPortalWidth / 2));
-
             int topBoundary = (int)(drawable.client.thisPlayer.Y - (drawable.client.currViewPortalWidth / 2));
 
             world_x = (int)((screen_x / 800 * drawable.client.currViewPortalWidth) + leftBoundary);
             world_y = (int)((screen_y / 800 * drawable.client.currViewPortalWidth) + topBoundary);
         }
 
+        /// <summary>
+        /// This method allows a player to split when they hit the space bar.
+        /// </summary>
+        /// <param name="sender"> the sender </param>
+        /// <param name="e"> the event args </param>
         private void OnEntryTextChanged(object sender, EventArgs e)
         {
             Entry space = (Entry) sender;
@@ -97,6 +154,12 @@ namespace ClientGUI
             Dispatcher.Dispatch(() => { space.Text = ""; });
         }
 
+        /// <summary>
+        /// This method ensures that the world is redrawn 30 frames per second and that 
+        /// the player is constantly asking to move toward their mouse position.
+        /// </summary>
+        /// <param name="source"> the source </param>
+        /// <param name="e"> the event args </param>
         private void TickEvent(Object source, ElapsedEventArgs e)
         {
             // redraw the world
@@ -106,6 +169,7 @@ namespace ClientGUI
             {
                 try
                 {
+                    // update the stats display
                     Dispatcher.Dispatch(() => { FoodLabel.Text = "Food: " + drawable.client.world.foods.Count; });
                     Dispatcher.Dispatch(() => { PositionLabel.Text = "Position: " + (int) drawable.client.thisPlayer.X + ", " + (int) drawable.client.thisPlayer.Y; });
                     Dispatcher.Dispatch(() => { MassLabel.Text = "Mass: " + drawable.client.thisPlayer.Mass; });
@@ -126,21 +190,49 @@ namespace ClientGUI
             }
         }
 
+        /// <summary>
+        /// This is the connection delegate to be passed into the Networking object, which 
+        /// ensures that when a client successfully connects to the server, they ask to join 
+        /// the game.
+        /// </summary>
+        /// <param name="networking"> the networking object that connected </param>
         private void OnConnect(Networking networking)
         {
-            // ask to start the game
             String message = String.Format(Protocols.CMD_Start_Game, NameEntry.Text);
             channel.Send(message);
         }
 
+        /// <summary>
+        /// This is the disconnection delegate to be passed into the Networking object, which 
+        /// simply provides helpful logging information.
+        /// </summary>
+        /// <param name="networking"> the networking object that disconnected </param>
         private void OnDisconnect(Networking networking)
         {
-
+            Debug.WriteLine("player successfully disconnected.");
         }
 
+        /// <summary>
+        /// This is the message received delegate to be passed into the Networking object, which 
+        /// checks for each of the specific commands from the server and handles them accordingly.
+        /// 
+        /// If receives the Command Food command: adds the new food to the food list and display
+        /// 
+        /// If receives the Command Eaten Food command: removes the eaten food from the foods list and display
+        /// 
+        /// If receives the Command Player Object command: link the client id to the given player id 
+        /// and set thisPlayer accordingly
+        /// 
+        /// If receives the Command Players command: adds the new food to the food list and display
+        /// 
+        /// If receives the Command Dead Players command: removes the dead players from the players list and display
+        /// 
+        /// </summary>
+        /// <param name="networking"> the networking object that sent the message </param>
+        /// <param name="message"> the message </param>
         private async void OnMessageReceived(Networking networking, string message)
         {
-            // {Command Food}
+            // example of command: {Command Food}
             if (message.Contains(Protocols.CMD_Food))
             {
                 string foodList = message.Substring(14);
@@ -159,7 +251,8 @@ namespace ClientGUI
                 }
             }
 
-            // {Command Player Object}5, lets the client know the game has started
+            // example of command: {Command Player Object}5
+            // lets the client know the game has started
             else if (message.Contains(Protocols.CMD_Player_Object))
             {
                 stopWatch.Start();
@@ -181,7 +274,7 @@ namespace ClientGUI
             }
 
 
-            // {Command Players}
+            // example of command: {Command Players}
             else if (message.Contains(Protocols.CMD_Update_Players))
             {
                 string playersList = message.Substring(17);
@@ -199,6 +292,7 @@ namespace ClientGUI
 
                         else
                         {
+                            // if the new player, is "this player" set thisPlayer accordingly
                             if (drawable.client.thisPlayer != null && player.ID == drawable.client.thisPlayer.ID)
                             {
                                 drawable.client.thisPlayer = player;
@@ -212,10 +306,10 @@ namespace ClientGUI
             }
 
 
-            // {Command Dead Players}[5,10,20,30,16,121,...]
+            // example of command: {Command Dead Players}[5,10,20,30,16,121,...]
             else if (message.Contains(Protocols.CMD_Dead_Players))
             {
-                // this will be [5,10,20,30,16,121,...]
+                // parse the string of ids into a list of separate ids
                 string deadPlayerIDs = message.Substring(22);
                 deadPlayerIDs = deadPlayerIDs.Replace("[", String.Empty);
                 deadPlayerIDs = deadPlayerIDs.Replace("]", String.Empty);
@@ -243,6 +337,7 @@ namespace ClientGUI
                         }
                     }
 
+                    // if "this player" has died, present their stats and ask if they want to play again
                     if (id == drawable.client.thisPlayer.ID)
                     {
                         stopWatch.Stop();
@@ -268,10 +363,10 @@ namespace ClientGUI
             }
 
 
-            // {Command Eaten Food}[2701,2546,515,1484,2221,240,1378,1124,1906,1949]
+            // example of command: {Command Eaten Food}[2701,2546,515,1484,2221,240,1378,1124,1906,1949]
             else if (message.Contains(Protocols.CMD_Eaten_Food))
             {
-                // this will be [2701,2546,515,1484,2221,240,1378,1124,1906,1949]
+                // parse the string of ids into a list of separate ids
                 string eatenFood = message.Substring(20);
                 eatenFood = eatenFood.Replace("[", String.Empty);
                 eatenFood = eatenFood.Replace("]", String.Empty);
